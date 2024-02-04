27aefc5ffe917107cb4f65ac3e06e5a3fbc242ef
Ben Kallus <benjamin.p.kallus.gr@dartmouth.edu>
2024-02-03 20:19:52 -0500

    Make linter run-dir agnostic

--- tools/lint.sh
+++ tools/lint.sh
@@ -1,1 +1,3 @@
+cd "$(dirname "$0")"
+
 black -l 110 *.py && mypy *.py && pylint --disable=arguments-out-of-order,too-many-instance-attributes,line-too-long,invalid-name,too-many-branches,broad-exception-caught,missing-function-docstring,missing-module-docstring,missing-class-docstring,raise-missing-from,too-many-boolean-expressions,too-many-nested-blocks,too-many-statements,too-many-locals *.py
