9d9f5cd432f56082f4a5862756e4dfe2e38119b0
Ben Kallus <benjamin.p.kallus.gr@dartmouth.edu>
2024-02-23 10:45:51 -0500

    Update echo server to match CDN version

--- images/echo_server/server.py
+++ images/echo_server/server.py
@@ -2,10 +2,9 @@
 
 import socket
 import threading
-import sys
 
-RECV_SIZE: int = 65536
-SOCKET_TIMEOUT:float = 0.1
+RECV_SIZE = 65536
+SOCKET_TIMEOUT = .1
 
 
 def really_recv(sock: socket.socket) -> bytes:
@@ -14,17 +13,17 @@ def really_recv(sock: socket.socket) -> bytes:
     try:
         while b := sock.recv(RECV_SIZE):
             result += b
-    except TimeoutError:
+    except (TimeoutError, ConnectionResetError):
         pass
     return result
 
 
-def handle_connection(client_sock: socket.socket) -> None:
+def handle_connection(client_sock: socket.socket, _client_address: tuple[str, int]) -> None:
     client_sock.settimeout(SOCKET_TIMEOUT)
     while payload := really_recv(client_sock):
         try:
             client_sock.sendall(
-                f"HTTP/1.1 200 OK\r\nContent-Length: {len(payload)}\r\n\r\n".encode("ascii") + payload
+                    f"HTTP/1.1 200 OK\r\nCache-Control: public, max-age=2592000\r\nServer: echo-python\r\nContent-Length: {len(payload)}\r\n\r\n".encode("ascii") + payload
             )
         except ConnectionResetError:
             pass
@@ -37,8 +36,7 @@ def main() -> None:
     server_sock.bind(("0.0.0.0", 80))
     server_sock.listen()
     while True:
-        client_sock, _ = server_sock.accept()
-        t: threading.Thread = threading.Thread(target=handle_connection, args=(client_sock,))
+        t: threading.Thread = threading.Thread(target=handle_connection, args=(*server_sock.accept(),))
         t.start()
 
 if __name__ == "__main__":
