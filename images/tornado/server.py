c3526ff204a7add22952b4cc574c03c44e02efd2
Ben Kallus <benjamin.p.kallus.gr@dartmouth.edu>
2024-07-16 16:10:32 -0400

    Re-add tornado

---
+++ images/tornado/server.py
@@ -0,0 +1,62 @@
+import asyncio
+import base64
+
+import tornado
+
+
+class BagOfEverything:
+    def __contains__(self, _):
+        return True
+
+
+class Handler(tornado.web.RequestHandler):
+    SUPPORTED_METHODS = BagOfEverything()
+    _new_cookie = {}
+
+    def __getattr__(self, key: str):
+        return self._handle
+
+    def _handle(self, *args, **kwargs) -> None:
+        method: bytes = base64.b64encode(self.request.method.encode("latin1"))
+        version: bytes = base64.b64encode(self.request.version.encode("latin1"))
+        uri: bytes = base64.b64encode(self.request.uri.encode("latin1"))
+        body: bytes = base64.b64encode(self.request.body)
+
+        response_body: bytes = (
+            b'{"headers":['
+            + b",".join(
+                b'["'
+                + base64.b64encode(k.encode("latin1"))
+                + b'","'
+                + base64.b64encode(v.encode("latin1"))
+                + b'"]'
+                for k, v in self.request.headers.get_all()
+            )
+            + b'],"body":"'
+            + body
+            + b'","method":"'
+            + method
+            + b'","uri":"'
+            + uri
+            + b'","version":"'
+            + version
+            + b'"}'
+        )
+        self.write(response_body)
+
+    # This has to be special-cased because tornado/web.py:275-281 sets them to unimplemented
+    head = get = post = delete = patch = put = options = _handle
+
+
+async def main():
+    app = tornado.web.Application([(r".*", Handler)])
+    app.listen(80)
+    shutdown_event = asyncio.Event()
+    await shutdown_event.wait()
+
+
+if __name__ == "__main__":
+    import afl
+
+    afl.init()
+    asyncio.run(main())
