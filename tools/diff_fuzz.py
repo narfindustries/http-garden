42a5a1c0922b13d40a2f70603eefeccd21bd6cfa
Ben Kallus <benjamin.p.kallus.gr@dartmouth.edu>
2024-08-28 17:58:13 -0400

    Change seeds a little bit

--- tools/diff_fuzz.py
+++ tools/diff_fuzz.py
@@ -18,14 +18,13 @@ from targets import Service
 from util import stream_t, unzip, fingerprint_t, translate
 
 _MIN_GENERATION_SIZE: Final[int] = 10
+
+# These are the requests that the fuzzer starts with
 SEEDS: Final[list[stream_t]] = [
     [b"GET / HTTP/1.1\r\n\r\n"],
     [b"POST / HTTP/1.1\r\nContent-Length: 10\r\nHost: b\r\n\r\n0123456789"],
-    [
-        b"POST / HTTP/1.1\r\nHost: c\r\nTransfer-Encoding: chunked\r\n\r\n5\r\n01234\r\n5\r\n56789\r\n0\r\n\r\n"
-    ],
+    [b"POST / HTTP/1.1\r\nHost: c\r\nTransfer-Encoding: chunked\r\n\r\n5\r\n01234\r\n0\r\n\r\n"],
 ]
-
 
 def normalize_request_wrt_response(r: HTTPRequest, s: Service) -> HTTPRequest:
     # If s added headers to r, then remove them.
