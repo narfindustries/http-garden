bbdfd352937b138b9050367aad9a6f2fa7fccdad
Ben Kallus <benjamin.p.kallus.gr@dartmouth.edu>
2024-08-31 14:33:19 -0400

    Catch some exceptions in server.rb

--- images/protocol_http1/server.rb
+++ images/protocol_http1/server.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'base64'
 require 'json'
@@ -6,34 +6,38 @@ require 'socket'
 require 'protocol/http1/connection'
 require 'protocol/http/body/buffered'
 
-Addrinfo.tcp("0.0.0.0", 80).listen do |server|
-    loop do
-        client, address = server.accept
-        connection = Protocol::HTTP1::Connection.new(client)
+def handle_connection(connection)
+  loop do
+    request = connection.read_request
+    break unless request
 
-        # Read request:
-        while request = connection.read_request()
-            authority, method, path, version, headers, body_reader = request
-            body = body_reader ? body_reader.join() : ""
-            result = {
-                'headers': headers.fields.map {
-                    |k, v|
-                    [Base64.encode64(k).strip(), Base64.encode64(v).strip()]
-                } + (
-                    authority ?
-                    [[Base64.encode64("Host").strip(), Base64.encode64(authority).strip()]] :
-                    []
-                ),
-                'method': Base64.encode64(method).strip(),
-                'uri': Base64.encode64(path).strip(),
-                'version': Base64.encode64(version).strip(),
-                'body': Base64.encode64(body).strip()
-            }
-
-            connection.write_response(version, 200, [])
-            connection.write_body(version, Protocol::HTTP::Body::Buffered.wrap([result.to_json]))
-
-            break unless connection.persistent
-        end
+    authority, method, path, version, headers, body_reader = request
+    body = body_reader ? body_reader.join : ''
+    b64_headers = headers.fields.map do |k, v|
+      [Base64.encode64(k).strip, Base64.encode64(v).strip]
     end
+    b64_headers += [[Base64.encode64('Host').strip, Base64.encode64(authority).strip]] if authority
+    result = {
+      'headers': b64_headers,
+      'method': Base64.encode64(method).strip,
+      'uri': Base64.encode64(path).strip,
+      'version': Base64.encode64(version).strip,
+      'body': Base64.encode64(body).strip
+    }
+
+    connection.write_response(version, 200, [])
+    connection.write_body(version, Protocol::HTTP::Body::Buffered.wrap([result.to_json]))
+
+    break unless connection.persistent
+  rescue Protocol::HTTP1::InvalidRequest, Protocol::HTTP1::BadRequest, Protocol::HTTP1::BadHeader
+    break
+  end
+end
+
+Addrinfo.tcp('0.0.0.0', 80).listen do |server|
+  loop do
+    client, _address = server.accept
+    connection = Protocol::HTTP1::Connection.new(client)
+    handle_connection(connection)
+  end
 end
