93aad0e3406af35c3179782f0d56e4f941199f16
Ben Kallus <benjamin.p.kallus.gr@dartmouth.edu>
2025-01-31 11:13:31 -0500

    Format Pingora server

--- images/pingora/garden_gateway.rs
+++ images/pingora/garden_gateway.rs
@@ -26,30 +26,33 @@ use pingora_core::Result;
 use pingora_http::ResponseHeader;
 use pingora_proxy::{ProxyHttp, Session};
 
-pub struct MyGateway {
-}
+pub struct MyGateway {}
 
 #[async_trait]
 impl ProxyHttp for MyGateway {
     type CTX = ();
     fn new_ctx(&self) -> Self::CTX {}
 
-    async fn request_filter(&self, session: &mut Session, _ctx: &mut Self::CTX) -> Result<bool> {
+    async fn request_filter(&self, _session: &mut Session, _ctx: &mut Self::CTX) -> Result<bool> {
         Ok(false)
     }
 
     async fn upstream_peer(
         &self,
-        session: &mut Session,
+        _session: &mut Session,
         _ctx: &mut Self::CTX,
     ) -> Result<Box<HttpPeer>> {
-        Ok(Box::new(HttpPeer::new(("PROXY_BACKEND_PLACEHOLDER", 80), false, "PROXY_BACKEND_PLACEHOLDER".to_string())))
+        Ok(Box::new(HttpPeer::new(
+            ("PROXY_BACKEND_PLACEHOLDER", 80),
+            false,
+            "PROXY_BACKEND_PLACEHOLDER".to_string(),
+        )))
     }
 
     async fn response_filter(
         &self,
         _session: &mut Session,
-        upstream_response: &mut ResponseHeader,
+        _upstream_response: &mut ResponseHeader,
         _ctx: &mut Self::CTX,
     ) -> Result<()>
     where
@@ -60,24 +63,20 @@ impl ProxyHttp for MyGateway {
 
     async fn logging(
         &self,
-        session: &mut Session,
+        _session: &mut Session,
         _e: Option<&pingora_core::Error>,
-        ctx: &mut Self::CTX,
-    ) {}
+        _ctx: &mut Self::CTX,
+    ) {
+    }
 }
 
 fn main() {
     env_logger::init();
 
-    // read command line arguments
-    let opt = Opt::parse();
-    let mut my_server = Server::new(Some(opt)).unwrap();
+    let mut my_server = Server::new(Some(Opt::parse())).unwrap();
     my_server.bootstrap();
 
-    let mut my_proxy = pingora_proxy::http_proxy_service(
-        &my_server.configuration,
-        MyGateway {},
-    );
+    let mut my_proxy = pingora_proxy::http_proxy_service(&my_server.configuration, MyGateway {});
     my_proxy.add_tcp("0.0.0.0:80");
     my_server.add_service(my_proxy);
     my_server.run_forever();
