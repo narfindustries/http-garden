239e4b8c2d22b8558f3f6dbd77bfc5e3108d3703
Ben Kallus <benjamin.p.kallus.gr@dartmouth.edu>
2025-09-01 19:13:28 -0400

    Make node_stdlib support h1 and h2 on the same socket (requires tls though)

--- images/node_stdlib/app.js
+++ images/node_stdlib/app.js
@@ -1,14 +1,32 @@
-require('http').createServer((request, response) => {
-    let body = [];
-    request.on('data', (chunk) => {
-        body.push(chunk);
-    }).on('end', () => {
-        response.end(JSON.stringify({
-            "headers": Object.keys(request.headers).map((key) => [btoa(key), btoa(request.headers[key])]),
-            "body": Buffer.concat(body).toString('base64'),
-            "uri": btoa(request.url),
-            "method": btoa(request.method),
-            "version": btoa(request.httpVersion),
-        }));
-    });
-}).listen(80);
+const fs = require("fs");
+
+require("http2")
+  .createSecureServer(
+    {
+      key: fs.readFileSync("/app/garden.crt.key"),
+      cert: fs.readFileSync("/app/garden.crt"),
+      allowHTTP1: true,
+    },
+    (request, response) => {
+      let body = [];
+      request
+        .on("data", (chunk) => {
+          body.push(chunk);
+        })
+        .on("end", () => {
+          response.end(
+            JSON.stringify({
+              headers: Object.keys(request.headers).map((key) => [
+                btoa(key),
+                btoa(request.headers[key]),
+              ]),
+              body: Buffer.concat(body).toString("base64"),
+              uri: btoa(request.url),
+              method: btoa(request.method),
+              version: btoa(request.httpVersion),
+            }),
+          );
+        });
+    },
+  )
+  .listen(443);
